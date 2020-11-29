1ae21038a2806c4155104950c07254158adf8665
abhilash1408 <abhilashthot99@gmail.com>
2020-11-29 22:21:56 +0530

    create home page ui -fixes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,15 +7,19 @@ function Home(props) {
         <div className="background metal-font">
             <Container className='home-container'>
                 <Row>
-                    <Col xs={7} className='my-auto'>
+                    <Col xs={6} className='my-auto'>
                         <Col>
-                            <Button variant="outline-dark" className='custom-button'>Single playe<span>r</span></Button>
+                            <Row className='justify-content-end'>
+                                <Button variant="outline-dark" className='custom-button'>Single playe<span>r</span></Button>
+                            </Row>
                         </Col>
                         <Col>
-                            <Button variant="outline-dark" className='custom-button'>Double playe<span>r</span></Button>
+                            <Row className='justify-content-end'>
+                                <Button variant="outline-dark" className='custom-button'>Double playe<span>r</span></Button>
+                            </Row>
                         </Col>
                     </Col>
-                    <Col xs={5}><img src={snake} alt='snake'></img></Col>
+                    <Col xs={6}><img src={snake} alt='snake'></img></Col>
                 </Row>
                 <Row className='justify-content-center'>
                     <h1>Solid snak<span>e</span></h1>
