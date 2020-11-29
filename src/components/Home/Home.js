c912ba7e90239a8c6de374ff80242499f03dc220
abhilash1408 <abhilashthot99@gmail.com>
2020-11-29 22:10:50 +0530

    create home page ui

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import './Home.css';
 import {Container,Row,Col,Button} from 'react-bootstrap';
+import snake from '../../assets/images/snake.png';
 function Home(props) {
     return (
         <div className="background metal-font">
-            <Container>
+            <Container className='home-container'>
                 <Row>
-                    
-                    <Col xs={7}>   <Button variant="outline-dark">Single player</Button></Col>
-                    <Col xs={5}> (snake)</Col>
+                    <Col xs={7} className='my-auto'>
+                        <Col>
+                            <Button variant="outline-dark" className='custom-button'>Single playe<span>r</span></Button>
+                        </Col>
+                        <Col>
+                            <Button variant="outline-dark" className='custom-button'>Double playe<span>r</span></Button>
+                        </Col>
+                    </Col>
+                    <Col xs={5}><img src={snake} alt='snake'></img></Col>
                 </Row>
-               <span >Solid Snake</span> 
+                <Row className='justify-content-center'>
+                    <h1>Solid snak<span>e</span></h1>
+                </Row>
+                <Row className='justify-content-center'>
+                    <p>HE WHO CONTROLS THE BATTLEFIELD, CONTROLS HISTOR<span>Y</span></p>
+                </Row>
 
             </Container>
         </div>
